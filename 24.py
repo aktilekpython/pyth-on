3ede39606d0f3096e4d4922f13229eb99c1d4fc1
Your Name <zhumagulovaktilek655@gmail.com>
2020-10-24 12:51:24 +0600

    first

---
+++ 24.py
@@ -0,0 +1,57 @@
+#method  словари
+
+#dict_with_pairs = {"эключ1": значание}
+#dict_ = {3: 1, 3.4: 2, 3: 2}
+#print(dict_)
+
+#method fromkeys
+
+#names = {'name1': 'Jonh':  'name2': 'Tom'}
+#print(names['name1'])
+#print(names['name2'])
+
+#dictionary dict.fromkeys(['key1', 'key2'])
+#print(dictionery)
+#dictionery2 dict.fromkeys(['key1', ])
+#print(dictionery2)
+
+#method get  получает по ключу значение
+# cars = {'mersedes': '221', 'bmv': '750', 'subaru': 'impreza'}
+# print(cars.keys())
+# cars_list = cars.keys()
+# print(cars_list)
+# print(len(cars))
+# mersedes = cars.get('mersedes')
+# print(mersedes)
+
+#method values  вывволит все значение
+
+# cars = {'mersedes': '221', 'bmv': '750', 'subaru': 'impreza'}
+# print(cars.values())
+# print(type(cars.values()))
+
+
+#method pop удаляет по ключе
+
+
+#dict_ = {'first': '1st', 'second': '2nd ',  }
+
+
+
+
+
+
+#menhod popoitem
+# cars = {'mersedes': '221', 'bmv': '750', 'subaru': 'impreza'}
+# deleted = cars.popitem()
+# print(deleted)
+# print(cars)
+
+# method  мноожество
+
+set_ = {1,2,3,4,5,6,7,8,9,0}
+print(set_.pop())
+print(set_)
+
+
+
